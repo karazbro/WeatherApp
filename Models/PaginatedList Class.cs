3e86ee365be345030248fc77b2f1b02455da050e
Krasnov Danil <d-krasnov@asoft21.ru>
2025-03-02 20:33:15 +0300

    fix

--- Models/PaginatedList Class.cs
+++ Models/PaginatedList Class.cs
@@ -27,4 +27,9 @@ namespace WeatherApp.Models
 
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
-            var count = await source
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
